1c34ff9d993a6215cc6b38a4e8f8df57fa8bf56e
Sohail Bhamani <sohail@gmail.com>
2018-08-02 17:45:43 -0500

    fixed checkstyule warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+ /**
+  * @param somone the name of a person
+  * @return greeting string
+  */
+  public String final greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
